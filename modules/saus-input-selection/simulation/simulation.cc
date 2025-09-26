b855a2efe508d7c56988ab186942b4ca2aac3e58
Abel Scheidt <162272483+abel-scheidt@users.noreply.github.com>
2025-09-26 11:34:47 -0300

    simulation.cc now supports different transform types

--- modules/saus-input-selection/simulation/simulation.cc
+++ modules/saus-input-selection/simulation/simulation.cc
@@ -2,6 +2,10 @@
 
 using namespace std;
 
+#define dct2 "00"
+#define dst7 "01"
+#define dct8 "10"
+
 void print_vector(array<int, 32> &input_vector, string name) {
     cout << "-" << name << "-" << endl;
     for (int i = 0; i < 32; i++) {
@@ -12,7 +16,6 @@ void print_vector(array<int, 32> &input_vector, string name) {
 }
 
 // Nota: Caso N=4, nao altera o vetor
-// Funcionando!
 array<int, 32> t2s(array<int, 32> &input_vector, int N, int depth) {
     array<int, 32> rearranged_vector = input_vector;
 
@@ -34,7 +37,8 @@ array<int, 32> t2s(array<int, 32> &input_vector, int N, int depth) {
 }
 
 // Nota: Caso N=4, nao altera o vetor
-// Funcionando!
+// OBS: A ordem dos vetores não foi reversa. Como isso não
+// é importante para os cálculos, ignoramos nesse momento.
 array<int, 32> t2s_back(array<int, 32> &input_vector, int N) {
     array<int, 32> rearranged_vector = input_vector;
 
@@ -54,10 +58,10 @@ array<int, 32> t2s_back(array<int, 32> &input_vector, int N) {
 array<int, 32> decompose(array<int, 32> input_vector, int N) {
     int max_depth = __builtin_ffs(N) - 2;
     for (int depth = 0; depth < max_depth; depth++) {
-        cout << "\nDecomposing... Current depth = " << depth << endl;
-        print_vector(input_vector, "Raw vector");
+        //cout << "\nDecomposing... Current depth = " << depth << endl;
+        //print_vector(input_vector, "Raw vector");
         input_vector = t2s(input_vector, N, depth);
-        print_vector(input_vector, "T2s applied");
+        //print_vector(input_vector, "T2s applied");
 
         for (int pos = 0; pos < (32 / (1 << depth)); pos += 4) {
             array<int, 4> saved_vector;
@@ -67,26 +71,43 @@ array<int, 32> decompose(array<int, 32> input_vector, int N) {
             saved_vector[3] = input_vector[pos] - input_vector[pos+3];
             for (int i = 0; i < 4; i++) input_vector[pos+i] = saved_vector[i];
         }
-        print_vector(input_vector, "Sums and subtractions applied");
+        //print_vector(input_vector, "Sums and subtractions applied");
         input_vector = t2s_back(input_vector, 32 >> depth);
-        print_vector(input_vector, "T2s back applied");
+        //print_vector(input_vector, "T2s back applied");
         N /= 2;
     }
 
     return input_vector;
 }
 
-// Decomposição da DCT-II já está funcionando
-// Falta implementar seleção entre as 3 transformadas
-// Dúvida: esse módulo difere a DST-VII da DCT-VIII?
-int main() {
-    int N;
-    array<int, 32> input_vector, decomposed_vector;
+array<int, 32> reverse(array<int, 32> &input_vector, int N) {
+    array<int, 32> reversed_vector;
 
-    cin >> N;
+    for (int pos = 0; pos < 32; pos++) {
+        int new_pos = 32 - ((pos / N + 1) * N) + pos % N;
+        reversed_vector[new_pos] = input_vector[pos];
+    }
+
+    return reversed_vector;
+}
+
+// Dúvida: esse módulo difere a DST-VII da DCT-VIII?
+// Resposta: Sim! No projeto não fica claro se a ordem é invertida
+// Antes ou depois do módulo. Decidimos fazer a inversão dentro do módulo.
+int main() {
+    int N; string type;
+    array<int, 32> input_vector, output_vector;
+
+    cin >> N >> type;
     for (int i = 0; i < 32; i++) cin >> input_vector[i];
 
-    decomposed_vector = decompose(input_vector, N);
+    if (type == dct2) {
+        output_vector = decompose(input_vector, N);
+    } else if (type == dct8) {
+        output_vector = reverse(input_vector, N);
+    } else { // if type == dst7
+        output_vector = input_vector;
+    }
 
-    print_vector(decomposed_vector, "Decomposed Vector");
+    print_vector(output_vector, "Output Vector");
 }
