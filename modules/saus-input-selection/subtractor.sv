84949171d3b76d792754cdd9ea686741bf33b86a
Abel Scheidt <162272483+abel-scheidt@users.noreply.github.com>
2025-10-01 12:25:59 -0300

    Update subtractor.sv

--- modules/saus-input-selection/subtractor.sv
+++ modules/saus-input-selection/subtractor.sv
@@ -1,9 +1,9 @@
 module subtractor #(
     parameter WIDTH = 16
 )(
-    input  logic [WIDTH-1:0] a,
-    input  logic [WIDTH-1:0] b,
-    output logic [WIDTH:0] diff
+    input  logic signed [WIDTH-1:0] a,
+    input  logic signed [WIDTH-1:0] b,
+    output logic signed [WIDTH:0] diff
 );
     assign diff = a - b;
 endmodule
