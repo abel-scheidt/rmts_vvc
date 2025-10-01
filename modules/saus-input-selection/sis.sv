20a4ab02a380c2607c2a60c850682818af03dc7c
Abel Scheidt <162272483+abel-scheidt@users.noreply.github.com>
2025-10-01 12:09:39 -0300

    Definitio of params, inputs and output on sis.sv

--- modules/saus-input-selection/sis.sv
+++ modules/saus-input-selection/sis.sv
@@ -1,1 +1,9 @@
-
+module sis #(
+    parameter WIDTH = 16,
+    parameter MAX_N = 32
+)(
+    input  logic [1:0] transform_type,
+    input  logic [5:0] N,
+    input  logic [WIDTH-1:0] input_vector [31:0],
+    output logic [WIDTH-1:0] output_vector [31:0]
+);
