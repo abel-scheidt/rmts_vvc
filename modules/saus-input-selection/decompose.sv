1f55587e867bd56abbd60edc052cbc0490775c52
Abel Scheidt <162272483+abel-scheidt@users.noreply.github.com>
2025-10-01 12:26:49 -0300

    Update decompose.sv

--- modules/saus-input-selection/decompose.sv
+++ modules/saus-input-selection/decompose.sv
@@ -2,7 +2,7 @@ module decompose #(
     parameter WIDTH = 16,
     parameter MAX_N = 32
 )(
-    input  logic [WIDTH-1:0] input_vector [31:0],
+    input  logic signed [WIDTH-1:0] input_vector [31:0],
     input  logic [5:0] N,
-    output logic [WIDTH-1:0] output_vector [31:0]
+    output logic signed [WIDTH-1:0] output_vector [31:0]
 );
