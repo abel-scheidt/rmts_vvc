026f7e53cc43b659e556540925acc62b2c42f225
Abel Scheidt <162272483+abel-scheidt@users.noreply.github.com>
2025-10-01 12:27:17 -0300

    Update adder.sv

--- modules/saus-input-selection/adder.sv
+++ modules/saus-input-selection/adder.sv
@@ -1,9 +1,9 @@
 module adder #(
     parameter WIDTH = 16
 )(
-    input  logic [WIDTH-1:0] a,
-    input  logic [WIDTH-1:0] b,
-    output logic [WIDTH:0] sum
+    input  logic signed [WIDTH-1:0] a,
+    input  logic signed [WIDTH-1:0] b,
+    output logic signed [WIDTH:0] sum
 );
     assign sum = a + b;
 endmodule
