d0a4f583645328061f12a21452b2089dece1de79
Abel Scheidt <162272483+abel-scheidt@users.noreply.github.com>
2025-10-01 12:19:42 -0300

    Update t2s_back.sv

--- modules/saus-input-selection/t2s_back.sv
+++ modules/saus-input-selection/t2s_back.sv
@@ -2,6 +2,6 @@ module t2s_back #(
     parameter WIDTH = 16,
     parameter DEPTH = 0
 )(
-    input  logic [WIDTH-1:0] input_vector [31:0],
-    output logic [WIDTH-1:0] output_vector [31:0]
+    input  signed [WIDTH-1:0] input_vector [31:0],
+    output signed [WIDTH-1:0] output_vector [31:0]
 );
