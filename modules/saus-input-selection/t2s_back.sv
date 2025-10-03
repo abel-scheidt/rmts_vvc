b6d57dc62eed81a37736d29124f1066c81deb944
Abel Scheidt <162272483+abel-scheidt@users.noreply.github.com>
2025-10-03 10:44:10 -0300

    Update t2s_back.sv

--- modules/saus-input-selection/t2s_back.sv
+++ modules/saus-input-selection/t2s_back.sv
@@ -5,3 +5,22 @@ module t2s_back #(
     input  logic signed [WIDTH-1:0] input_vector [31:0],
     output logic signed [WIDTH-1:0] output_vector [31:0]
 );
+
+    always_comb begin
+        int N = 32 >> DEPTH;
+        
+        for (int pos = N; pos < 32; pos++) begin
+            output_vector[pos] = input_vector[pos];
+        end
+
+        int new_pos;
+        for (int pos = 0; pos < N; pos++) begin
+            if ((pos % 4) / 2) begin // Odd
+                new_pos = N / 2 + pos / 2 - pos % 2;
+            end else begin // Even
+                new_pos = pos / 2 + pos % 2;
+            end
+            output_vector[new_pos] = input_vector[pos];
+        end
+    end
+endmodule
