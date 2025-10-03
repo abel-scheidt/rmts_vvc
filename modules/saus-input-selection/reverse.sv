3894c842db899ba493119ec5b2d25d5288be2c05
Abel Scheidt <162272483+abel-scheidt@users.noreply.github.com>
2025-10-03 10:52:54 -0300

    Create reverse.sv

---
+++ modules/saus-input-selection/reverse.sv
@@ -0,0 +1,15 @@
+module reverse #(
+    parameter WIDTH = 16,
+    parameter N = 32
+)(
+    input  logic [WIDTH-1:0] input_vector [31:0],
+    output logic [WIDTH-1:0] output_vector [31:0]
+);
+    always_comb begin
+        int new_pos;
+        for (int pos = 0; pos < 32; pos++) begin
+            new_pos = 32 - ((pos / N + 1) * N) + pos % N;
+            output_vector[new_pos] = input_vector[pos];
+        end
+    end
+endmodule
